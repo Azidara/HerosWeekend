b377dd025e073f18c4a321e501cff80bb27a36fe
Azidara <albertbode@mac.com>
2023-06-16 21:09:11 -0500

    changes before merge

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public float height = -10;
+    public GameObject target = null;
+
+    void Start(){
+        if (target == null){
+            target = HWeekend.Client_State.getInstance.c.gameObject;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.transform.position + new Vector3(0,0,height);
+    }
+}
